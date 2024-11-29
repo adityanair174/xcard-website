434dd01944498037a33b8ae3df957d01f3ed6b8e
adityanair174 <57316811+adityanair174@users.noreply.github.com>
2024-11-29 23:30:09 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { Activity, BarChart2, MapPin, PlayCircle, Smartphone, Users } from 'lucide-react'
 import Link from "next/link"
-import Image from "next/image"
 
 export default function LandingPage() {
   return (
